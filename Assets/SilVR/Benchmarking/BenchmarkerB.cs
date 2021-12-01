70cc11161accfbf23988daa327e3363d82bcff67
Blake447 <BlakeAtk44@gmail.com>
2021-11-30 23:00:09 -0800

    Added SilVR assets

---
+++ Assets/SilVR/Benchmarking/BenchmarkerB.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class BenchmarkerB : UdonSharpBehaviour
+{
+    public int value = 0;
+    public int steps_per_frame = 10000;
+    private void Update()
+    {
+        for (int i = 0; i < steps_per_frame; i++)
+        {
+            int a = Random.Range(0, 8192);
+            int b = Random.Range(0, 8192);
+            value = Mathf.Max(a, b);
+        }
+    }
+}
